18af368731499dd32f35a5ab0ed64ca36042e104
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2024-05-06 23:25:12 +0300

    F

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { SocialMediaAndCopyrightsComponent } from "./shared/social-media-and-copyrights/social-media-and-copyrights.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, FooterComponent, NavbarComponent, SocialMediaAndCopyrightsComponent]
 })
 export class AppComponent {
   title = 'Lacasa-Residence';
---
+++ src/app/home/home-module/home-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModuleComponent } from './home-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeModuleComponent]
+})
+export class HomeModuleModule { }
