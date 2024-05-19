b44cd4dc3039e328c43e0cae4afd9ea28f9e17ec
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2024-05-20 00:15:14 +0300

    F

--- src/app/shared/social-media-and-copyrights/social-media-and-copyrights.component.ts
+++ src/app/shared/social-media-and-copyrights/social-media-and-copyrights.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 @Component({
   selector: 'app-social-media-and-copyrights',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './social-media-and-copyrights.component.html',
   styleUrl: './social-media-and-copyrights.component.scss'
 })
