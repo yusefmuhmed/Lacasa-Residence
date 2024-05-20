a963128afe8a4db1c35275fca00be5bd1d4fefa4
youssef.abdelmeged <youssef.abdelmeged@visionvalley.net>
2024-05-20 14:17:25 +0300

    Add trending section

--- src/app/shared/social-media-and-copyrights/social-media-and-copyrights.component.ts
+++ src/app/shared/social-media-and-copyrights/social-media-and-copyrights.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 
 @Component({
   selector: 'app-social-media-and-copyrights',
   standalone: true,
-  imports: [FontAwesomeModule],
   templateUrl: './social-media-and-copyrights.component.html',
   styleUrl: './social-media-and-copyrights.component.scss'
 })
